9795aad1cee3d3694d05931ac5ea59ec5ad29f84
Nikolas Andrew <nikolasandrew2021@gmail.com>
2024-07-11 16:51:39 -0300

    script feito

---
+++ js/script.js
@@ -0,0 +1,43 @@
+//ativar link do menu
+
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link){
+    const url = location.href;
+    const href = link.href;
+    if (url.includes(href)){
+        link.classList.add('ativo')
+    }
+}
+
+links.forEach(ativarLink);
+
+//ativar links do orçamento
+
+const parametros =  new URLSearchParams (location.search);
+
+function ativarProdutos(parametro){
+    const elemento = document.getElementById(parametro)
+    if (elemento){
+        elemento.checked = true
+    }
+}
+
+parametros.forEach(ativarProdutos);
+
+//Galeria de bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaconteiner = document.querySelector(".bicicleta-imagens");
+
+function trocarImagens(event){
+    const img = event.currentTarget
+    galeriaconteiner.prepend(img)
+    console.log(img)
+}
+
+function eventosGaleria(img){
+    img.addEventListener('click', trocarImagens)
+}
+
+galeria.forEach(eventosGaleria);
